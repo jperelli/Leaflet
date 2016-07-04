fd35680eeb1f20b5e5c4d1b0bcbf671a521f77fd
Yohan Boniface <yb@enix.org>
2016-07-04 22:49:40 +0200

    Make L.Handler.enable/disable return this
    Those are methods used very often, this can save us a lot of
    useless lines.

--- src/core/Handler.js
+++ src/core/Handler.js
@@ -15,19 +15,21 @@ L.Handler = L.Class.extend({
 	// @method enable()
 	// Enables the handler
 	enable: function () {
-		if (this._enabled) { return; }
+		if (this._enabled) { return this; }
 
 		this._enabled = true;
 		this.addHooks();
+		return this;
 	},
 
 	// @method disable()
 	// Disables the handler
 	disable: function () {
-		if (!this._enabled) { return; }
+		if (!this._enabled) { return this; }
 
 		this._enabled = false;
 		this.removeHooks();
+		return this;
 	},
 
 	// @method enabled(): Boolean
